70fc3349b5b80d765d47259deb008fb84fe29aa9
Mohye <dragonsinmeliodas777@gmail.com>
2024-05-29 16:14:46 +0300

    configure deployment files

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -8,19 +8,19 @@ use Illuminate\Validation\ValidationException;
 
 class NewsletterController extends Controller
 {
-   public function __invoke(Newsletter $newsletter)
-   {
-    request()->validate(['email' => 'required|email']);
+    public function __invoke(Newsletter $newsletter)
+    {
+        request()->validate(['email' => 'required|email']);
 
-    try {
-        $newsletter->subscribe(request('email'));
-    } catch (Exception $e) {
-        throw ValidationException::withMessages([
-            'email' => 'This email could not be added to our newsletter list.'
-        ]);
+        try {
+            $newsletter->subscribe(request('email'));
+        } catch (Exception $e) {
+            throw ValidationException::withMessages([
+                'email' => 'This email could not be added to our newsletter list.'
+            ]);
+        }
+
+        return redirect('/')
+            ->with('success', 'You are now signed up for our newsletter!');
     }
-
-    return redirect('/')
-        ->with('success', 'You are now signed up for our newsletter!');
-   }
 }
--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,7 +2,7 @@
 echo "Running composer"
 cp /etc/secrets/.env .env
 composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+composer install  --working-dir=/var/www/html
 
 echo "Clearing caches..."
 php artisan optimize:clear
