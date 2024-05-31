080f4a3035230fea6151acc9ed20d2cc7fd77a35
Mohye <dragonsinmeliodas777@gmail.com>
2024-05-31 15:41:37 +0300

    configure deployment files

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -14,7 +14,10 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate:fresh --force
+
+echo "seeding data"
+php artisan db:seed
 
 echo "done deploying"
 
